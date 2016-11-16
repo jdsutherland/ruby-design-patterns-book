aaac0387b1f0b652805d34237a0dae585ea9a516
Jeff Sutherland <jdsuther@asu.edu>
2016-11-16 19:20:10 +0700

    Refactor use duck typing rather than abstract base

--- ch4-strategy/formatter.rb
+++ ch4-strategy/formatter.rb
@@ -1,52 +1,42 @@
-# Different from Template Pattern:
-# We isolate the varying method in a new class.
-# We could create a whole family of classes for each variation.
-class Formatter
-  def output_report( title, text )
-    raise 'Abstract method called'
-  end
-end
-
-class HTMLFormatter < Formatter
-  def output_report(context)
-    puts('<html>')
-    puts('  <head>')
-    puts("    <title>#{context.title}</title>")
-    puts('  </head>')
-    puts('  <body>')
-    context.text.each do |line|
-      puts("    <p>#{line}</p>" )
-    end
-    puts('  </body>')
-    puts('</html>')
-  end
-end
-
-class PlainTextFormatter < Formatter
-  def output_report(context)
-    puts("***** #{context.title} *****")
-    context.text.each do |line|
-      puts(line)
-    end
-  end
-end
-
-# Report is much more simple
-class Report # called 'context' class
+class Report
   attr_reader :title, :text
   attr_accessor :formatter
 
   def initialize(formatter)
     @title = 'Monthly Report'
-    @text =  [ 'Things are going', 'really, really well.' ]
+    @text =  ['Things are going', 'really, really well.']
     @formatter = formatter
   end
 
-  def output_report
+  def output_report()
     @formatter.output_report(self)
   end
 end
 
-# example usage
-report = Report.new(HTMLFormatter.new)
-report.output_report
+class HTMLFormatter
+  def output_report(context)
+    puts('<html>')
+    puts('  <head>')
+    # Output The rest of the report ...
+    puts("    <title>#{context.title}</title>")
+    puts('  </head>')
+    puts('  <body>')
+
+    context.text.each do |line|
+      puts("    <p>#{line}</p>")
+    end
+
+    puts('  </body>')
+    puts('</html>')
+  end
+end
+
+class PlainTextFormatter
+  def output_report(context)
+    puts("***** #{context.title} *****")
+
+    context.text.each do |line|
+      puts(line)
+    end
+  end
+end

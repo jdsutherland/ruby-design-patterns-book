b3300475a5628a3ef2e084e4f4609d67a80ad620
Jeff Sutherland <jdsuther@asu.edu>
2016-12-01 17:09:04 +0700

    Add new observer class to demonstrate

--- ch5-observer/observer.rb
+++ ch5-observer/observer.rb
@@ -38,3 +38,12 @@ fred = Employee.new("Fred", "Crane Operator", 30000.0)
 payroll = Payroll.new
 fred.add_observer(payroll)
 fred.salary = 35000.0
+
+class TaxMan
+  def update(changed_employee)
+    puts("Send #{changed_employee.name} a new tax bill!")
+  end
+end
+
+tax_man = TaxMan.new
+fred.add_observer(tax_man)

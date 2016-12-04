d9bf510f195afa42e28f33a47d0d0a1f4c53f86d
Jeff Sutherland <jdsuther@asu.edu>
2016-12-04 15:31:45 +0700

    Refactor CompositeTask base class

--- ch6-composite/make_batter_task.rb
+++ ch6-composite/make_batter_task.rb
@@ -1,24 +1,8 @@
-# composite class
-class MakeBatterTask < Task
+class MakeBatterTask < CompositeTask
   def initialize
     super("Make batter")
-    @sub_tasks = []
     add_sub_task(AddDryIngredientsTask.new)
     add_sub_task(AddLiquidsTask.new)
     add_sub_task(MixTask.new)
-  end
-
-  def add_sub_task(task)
-    @sub_tasks << task
-  end
-
-  def remove_sub_task(task)
-    @sub_tasks.delete(task)
-  end
-
-  def get_time_required
-    time = 0.0
-    @sub_tasks.each { |task| time += task.get_time_required }
-    time
   end
 end

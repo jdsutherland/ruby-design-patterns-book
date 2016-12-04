4c5616b40f0e6a6644739d8e10e95569082e33a7
Jeff Sutherland <jdsuther@asu.edu>
2016-12-04 15:54:28 +0700

    Refactor overriding array operators & track parent
    We override array methods for convenience.
    Also, store parents so we can traverse up.

--- ch6-composite/composite_task.rb
+++ ch6-composite/composite_task.rb
@@ -6,10 +6,25 @@ class CompositeTask < Task
 
   def add_sub_task(task)
     @sub_tasks << task
+    task.parent = self
   end
 
   def remove_sub_task(task)
     @sub_tasks.delete(task)
+    task.parent = nil
+  end
+
+  alias << add_sub_task
+
+  def [](index)
+    @sub_tasks[index]
+  end
+
+  def []=(index, value)
+    replaced_value = @sub_tasks[index]
+    @sub_tasks[index] = value
+    replaced_value.parent = nil
+    value.parent = self
   end
 
   def get_time_required
--- ch6-composite/task.rb
+++ ch6-composite/task.rb
@@ -1,9 +1,10 @@
 # component class
 class Task
-  attr_reader :name
+  attr_reader :name, :parent
 
   def initialize(name)
     @name = name
+    @parent = nil
   end
 
   def get_time_required

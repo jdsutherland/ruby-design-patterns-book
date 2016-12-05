33b95a021bc88ca5125a4e71f09c52783d9c133a
Jeff Sutherland <jdsuther@asu.edu>
2016-12-05 15:44:08 +0700

    Add Commands that record
    We make use of the Composite pattern to record

---
+++ ch8-command/composite_command.rb
@@ -0,0 +1,36 @@
+require_relative "command"
+
+class CompositeCommand < Command
+  def initialize
+    @commands = []
+  end
+
+  def add_command(command)
+    @commands << command
+  end
+
+  alias << add_command
+
+  def remove_command(command)
+    @commands.delete(command)
+  end
+
+  def execute
+    @commands.each(&:execute)
+  end
+
+  def description
+    description = ""
+    @commands.each { |cmd| description += cmd.description + "\n" }
+    description
+  end
+end
+
+cmds = CompositeCommand.new
+
+cmds.add_command(CreateFile.new("file1.txt", "hello world\n"))
+cmds.add_command(CopyFile.new("file1.txt", "file2.txt"))
+cmds.add_command(DeleteFile.new("file1.txt"))
+
+cmds.execute
+puts(cmds.description)

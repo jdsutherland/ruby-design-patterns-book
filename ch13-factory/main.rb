34214dd5db75a2c9b650b1ebd8e65b3e71928efc
Jeff Sutherland <jdsuther@asu.edu>
2016-12-06 20:00:43 +0700

    Refactor to class-based OrganismFactory
    Add a Factory class that encapsulates the knowledge of which product
    types belong together

--- ch13-factory/main.rb
+++ ch13-factory/main.rb
@@ -1,9 +1,11 @@
-require_relative "jungle_organism_factory"
-require_relative "pond_organism_factory"
+require_relative "algae"
+require_relative "frog"
 require_relative "habitat"
+require_relative "jungle"
+require_relative "organism_factory"
 
-jungle = Habitat.new(1, 4, JungleOrganismFactory.new)
+jungle = Habitat.new(1, 4, OrganismFactory.new(Tree, Tiger))
 jungle.simulate_one_day
 
-pond = Habitat.new(2, 4, PondOrganismFactory.new)
+pond = Habitat.new(2, 4, OrganismFactory.new(Algae, Frog))
 pond.simulate_one_day

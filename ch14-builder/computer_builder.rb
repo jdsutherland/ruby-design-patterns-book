8061d72d261cf722420f9227fa03c478b4ef2c21
Jeff Sutherland <jdsuther@asu.edu>
2016-12-07 16:40:34 +0700

    Add example of "magic method" to make more elegant
    Sort of a hack way to streamline multiple creation calls like:
    builder.turbo
    builder.add_cd(true)
    builder.add_dvd
    builder.display = :lcd
    builder.add_hard_disk(100000)

--- ch14-builder/computer_builder.rb
+++ ch14-builder/computer_builder.rb
@@ -14,4 +14,20 @@ class ComputerBuilder
   def memory_size=(size_in_mb)
     @computer.memory_size = size_in_mb
   end
+
+  # this allows us to make calls like:
+  #
+  # builder.add_turbo_and_harddisk
+  # builder.add_turbo_and_dvd_and_harddisk
+  def method_missing(name, *args)
+    words = name.to_s.split("_")
+    return super(name, *args) unless words.shift == 'add'
+    words.each do |word|
+      next if word                  == 'and'
+      add_cd if word                == 'cd'
+      add_dvd if word               == 'dvd'
+      add_hard_disk(100000) if word == 'harddisk'
+      turbo if word                 == 'turbo'
+    end
+  end
 end
